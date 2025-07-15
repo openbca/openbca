152cd70a4ba85469a2326e2e7810b3d219834039
loic <loic.fontaine@recurve.com>
2025-07-15 13:17:45 -0700

    ensure quarter/month/hour_of_day are set

--- core/models/base/all_commodity_load_shape_ts.sql
+++ core/models/base/all_commodity_load_shape_ts.sql
@@ -6,17 +6,19 @@ MODEL(
 
 SELECT
     commodity::VARCHAR AS commodity,
-    quarter::INTEGER AS quarter,
-    month::INTEGER AS month,
-    hour_of_day::INTEGER AS hour_of_day,
+    COALESCE(quarter, FLOOR((hour_of_year - 1) / (8760 / 4)) + 1)::INTEGER AS quarter, -- TODO quarter/month calculation is approximate
+    COALESCE(month, FLOOR((hour_of_year - 1) / (8760 / 12)) + 1)::INTEGER AS month,
+    COALESCE(hour_of_day, (hour_of_year - 1) % 24)::INTEGER AS hour_of_day,
     hour_of_year::INTEGER AS hour_of_year,
-    load_shape::VARCHAR AS load_shape,
+    upper(load_shape::VARCHAR) AS load_shape,
     load_shape_normalized_fraction::FLOAT AS load_shape_normalized_fraction
 FROM (
-    SELECT * FROM openbca_reference.commodity_load_shape_ts
+    SELECT commodity, load_shape, hour_of_year, load_shape_normalized_fraction
+    FROM openbca_reference.commodity_load_shape_ts
     WHERE (commodity, load_shape) NOT IN (
         SELECT commodity, load_shape FROM openbca_input.load_shape_ts
     )
     UNION ALL
-    SELECT * FROM openbca_input.load_shape_ts
+    SELECT commodity, load_shape, hour_of_year, load_shape_normalized_fraction
+    FROM openbca_input.load_shape_ts
 )
